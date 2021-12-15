d20a49feec69f7e61e86c9cbcb2aaad1eb600baf
1unasy <96099990+1unasy@users.noreply.github.com>
2021-12-15 15:29:21 +0900

    블로그 상세 페이지 만들기
    - 블로그 post 상세 페이지 띄우기
    : single_post_page() 함수
    - 포스트 제목에 링크 만들기
    : {{p.title}} 수정 in <index.html>
    -> get_absoulte_url() 함수 정의 in <models.py>
    - 대문 페이지와 자기소개 페이지 만들기
    : single_pages 앱 사용
    : 대문 페이지와 자기소개 페이지의 URL 지정

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def index(request):
 
     # select * from Post; 와 동일 -> ORM
@@ -12,5 +13,18 @@ def index(request):
         'blog/index.html',
         {
             'posts' : posts
+        }
+    )
+
+# pk 같이 클라이언트가 서버에 보내는 데이터 : parameter
+# post, posts 등 서버가 클라이언트에 보내는 데이터 : attribute
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post' : post
         }
     )
