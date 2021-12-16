758911b839ca08db50fe8f924d4a3705e1c073b3
stella <tldus8267@gmail.com>
2021-12-16 13:01:55 +0900

    무신사 데이터 mysql에서 가져와서 웹페이지에 띄우기

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.landing),
-    path("about_me", views.about_me)
+    path("about_me", views.about_me),
+    path("musinsa", views.show_musinsa)
 ]
