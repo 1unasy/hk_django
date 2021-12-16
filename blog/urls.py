22d72f86c19198bbe916f9c23342e64f3a134b3a
stella <tldus8267@gmail.com>
2021-12-16 10:10:26 +0900

    CBV로 블로그 페이지 수정
    - ListView
    - DetailView

--- blog/urls.py
+++ blog/urls.py
@@ -3,13 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # PostList 추가하기
+    path("/", views.PostList.as_view()),
+
+    # PostDetail 추가하기
+    path("/<int:pk>/", views.PostDetail.as_view())
+
     # <ip: port>/blog : 블로그 메인 페이지
     # - 이미 hk_django_prj의 urls.py에 블로그에 들어가는 url 적어놓았기 때문 (포스트 목록 = 블로그 메인 p.161)
     # 도메인/blog/포스트 pk -> 포스트 pk = blog APP
     # 블로그 앱에 아무것도 안 치고 들어오면 views.index 함수를 보여줄거야.
-    path('/', views.index),
+    # path('/', views.index),
     # <ip : port>/blog/글 번호(pk)
     # <int:pk> = 정수 형태의 파라미터가 들어간다
     # /blog/1234 -> 1234를 숫자가 아닌 문자열 '1234'로 인식
-    path("/<int:pk>/", views.single_post_page)
+    # path("/<int:pk>/", views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,47 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+# '목록'을 보여주는 개념(ListView)에 알맹이(Post)를 넣겠다.
+class PostList(ListView):
+    # post_list 를 만든다.
+    # -> urls.py의 as_view() 함수기 post 목록을 다 가져온다.
+    model = Post
+    
+    # 장고의 ListView에서 자동으로 지정하는
+    # post_list.html을 사용하지 않고, index.html을 사용
+    # template_name = "blog/index.html"
 
-def index(request):
+    # 데이터 조회 정렬하기(내림차순)
+    ordering = "-pk"
 
-    # select * from Post; 와 동일 -> ORM
-    posts = Post.objects.all().order_by('-pk')
+class PostDetail(DetailView):
+    model = Post
 
-    return render(
-        request,
-        # django에서 알아서 /templates/를 앞에 붙여준다.
-        'blog/index.html',
-        {
-            'posts' : posts
-        }
-    )
+
+# def index(request):
+#
+#     # select * from Post; 와 동일 -> ORM
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         # django에서 알아서 /templates/를 앞에 붙여준다.
+#         'blog/index.html',
+#         {
+#             'posts' : posts
+#         }
+#     )
 
 # pk 같이 클라이언트가 서버에 보내는 데이터 : parameter
 # post, posts 등 서버가 클라이언트에 보내는 데이터 : attribute
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post' : post
+#         }
+#     )
